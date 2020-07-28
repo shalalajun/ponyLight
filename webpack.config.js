10a143dd8fc339a79a83bbfebcc7664b4dfbd430
shalalajun <shalalajun@naver.com>
2020-07-28 10:17:11 +0900

    fast08

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-    mode:"development",
+    mode:"production",
     entry:"./source/app.js",
     output:{
         path:path.resolve(__dirname,"public"),
